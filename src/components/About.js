69c2c74b4fea0158f17f3115316d6467dd7677fc
hazemfatnassi <fatnassi.hazem2005@gmail.com>
2025-11-19 03:45:28 +0100

    update

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ import PhoneIcon from '@mui/icons-material/Phone';
 const profileImageUrl = process.env.PUBLIC_URL + '/ft.jpg';
 
 const About = () => {
-  const cvUrl = process.env.PUBLIC_URL + '/CV Hazem fatnassi .pdf';
+  const cvUrl = process.env.PUBLIC_URL + '/cv hazem fatnassi ..pdf';
   const infoItems = [
     { icon: <LocationOnIcon color="primary" />, text: 'Ezzahra, 5199 Mahdia, Tunisie' },
     { icon: <EmailIcon color="primary" />, text: 'fatnassi.hazem2005@gmail.com' },
@@ -147,7 +147,7 @@ const About = () => {
                   <Button
                     component="a"
                     href={cvUrl}
-                    download="CV Hazem Fatnassi.pdf"
+                    download="cv hazem fatnassi ..pdf"
                     variant="outlined"
                     size="small"
                     sx={{
